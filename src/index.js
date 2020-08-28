b708ddffdee08587ab9c33ce2629944ac5d499b4
wathekk <wathek9711@gmail.com>
2020-08-28 18:14:13 +0100

    integrated now-ui-kit template to project and created a navbar with it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss?v=1.4.0";
+import "./assets/demo/demo.css?v=1.4.0";
+import "./assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+// pages for this kit
+import Index from "./Pages/Index.page.jsx";
+//import LoginPage from "./Pages/Login.page.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={(props) => <Index {...props} />} />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
